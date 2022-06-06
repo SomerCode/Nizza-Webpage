c4419e7cfd4d5b0dc7b7cc7e6807081507dc3f20
SomerCode <Geborsean@aol.com>
2022-06-07 01:30:55 +0200

    working preloading and google maps

--- response.js
+++ response.js
@@ -1,14 +1,18 @@
 let navopended = false
+let firstopen = true
+
 function opennav(){
     document.getElementById("navbar").style.width = "30%"
     document.getElementById("menubtn").style.visibility = "hidden"
     document.getElementById("top").style.marginLeft = "30%"
+    document.getElementById("main").style.marginLeft = "30%"
     navopended = true
 }
 function closenav(){
     document.getElementById("navbar").style.width = "0px"
     document.getElementById("menubtn").style.visibility = "visible"
     document.getElementById("top").style.marginLeft = "0px"
+    document.getElementById("main").style.marginLeft = "0%"
     navopended = false
 }
 function changesite(site){
